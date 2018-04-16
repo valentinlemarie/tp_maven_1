26324e8753fb90d26626035f9b1e01aa26af5741
valentinlemarie <valentin.lemarie@wanadoo.fr>
2018-04-16 11:59:46 +0200

    etape 7

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String asciiArt = FigletFont.convertOneLine("App");
+        System.out.println(asciiArt);
     }
 }
